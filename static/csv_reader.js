95c10092a1c3e19dc590605e256bd833b1aeca1e
Christoph Mittendorf <34183942+Cassini-chris@users.noreply.github.com>
2021-09-28 17:43:21 +0200

    Add files via upload

---
+++ static/csv_reader.js
@@ -0,0 +1,56 @@
+var full_text = [];
+
+
+function handleFiles(files) {
+  // Check for the various File API support.
+  if (window.FileReader) {
+      // FileReader are supported.
+      getAsText(files[0]);
+  } else {
+      alert('FileReader are not supported in this browser.');
+  }
+}
+
+function getAsText(fileToRead) {
+  var reader = new FileReader();
+  // Read file into memory as UTF-8
+  reader.readAsText(fileToRead);
+  // Handle errors load
+  reader.onload = loadHandler;
+  reader.onerror = errorHandler;
+}
+
+function loadHandler(event) {
+  var csv = event.target.result;
+  processData(csv);
+}
+
+function processData(csv) {
+    var allTextLines = csv.split(/\r\n|\n/);
+    var lines = [];
+    for (var i=0; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(';');
+            var tarr = [];
+            for (var j=0; j<data.length; j++) {
+                tarr.push(data[j]);
+            }
+            lines.push(tarr);
+    }
+  console.log(lines);
+    alert(lines);
+    full_text= lines;
+    var run_button = document.getElementById('run_button');
+    run_button.style.display = 'block';
+
+
+
+
+
+
+}
+
+function errorHandler(evt) {
+  if(evt.target.error.name == "NotReadableError") {
+      alert("Cannot read file !");
+  }
+}
